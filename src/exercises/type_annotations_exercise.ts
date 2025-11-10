bb972f217ba3d008c88329638ec63bbd7bdd9d5c
Veronika Pánková <pankova.veri@gmail.com>
2025-11-10 20:54:53 +0100

    type script user managmant

---
+++ src/exercises/type_annotations_exercise.ts
@@ -0,0 +1,13 @@
+/*
+Cvičení - typové anotace (⌛4:00)
+Vytvoř složku: src/exercises a v ní jednoduchý program:  type_annotations_exercise.ts, který:
+Vytvoří number proměnnou let age a tu inicializujte
+Změňte age na jiné číslo na dalším řádku
+Vypíše hodnotu proměnné age do terminálu
+Následně program spusť pomocí tsx
+*/
+
+let age: number;
+age = 23;
+age = 54;
+console.log("Věk" + age);
